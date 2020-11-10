4e0d4cbfa1f78c3ed8129d490f705100942faee4
Ricardo Madrid Henao <ricardo.madrid@qrvey.com>
2020-11-10 11:26:17 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# qrveydrsample-mongoDB-Loading
+# Introduction
+This sample shows how to use DataRouter to upload data into Qrvey platform. 
+
+Qrvey DataRouter provides developers with a means for loading their data from a variety of different sources into Qrvey’s high performance, ElasticSearch backend. DataRouter provides an API that can be used for creating metadata data mapping and custom transformations, live update data push, and large volume historical data loads.   It is designed to scale for all types of data volume and velocity scenarios.
+
+# Prerequisites
+Before using this sample, please have the following values
+1. Metadata URL. Contact help@qrvey.com if you don't have one.
+2. Postdata URL. Contact help@qrvey.com if you don't have one.
+3. Datarouter API Key
+4. AWS Account credentials (AccessKey and SecretKey) where DataRouter is installed.
+5. S3 bucket in AWS Account.
+
+# Installation
+
+1. Install NodeJS from https://nodejs.org (v12 recommended)
+2. Run the command to load data 
+
+# Steps
+
+## 1. Create Metadata
+
+Create the metadata specifying the fields and data types.
+
+Example:
+
+```
+curl --location --request POST '{{metadataurl}}/v5/metadata?publicConnection=true' \
+--header 'x-api-key: {{api-key}}' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "MetaDataId": "quick_start_index_name",
+    "indexName": "quick_start_index_name",
+    "skipOnPartialData": false,
+    "dateFormat": "YYYY-MM-DDTHH:mm:ss",
+    "columnType": [
+        {
+            "columnName": "CompanyId",
+            "type": "numeric-general"
+        },
+        {
+            "columnName": "Company Name",
+            "type": "text-label"
+        },
+        {
+            "columnName": "Foundation Date",
+            "type": "date"
+        }
+    ]
+}'
+```
+
+## 2. Create/Update a file called "config.json".
+
+In root folder with the following JSON:
+```
+{
+  "mongodburl": "",
+  "mongodbcollection": "",
+  "mongodbdatabase": "",
+  "mongodbquery": {},
+  "s3Bucket": "", //Bucket name created in AWS Account. See Prerequisites #5
+  "metadataId": "",
+  "postdataurl": "",
+  "apikey": ""
+}
+
+```
+
+# Usage
+
+Load Data: `node index.js`
---
+++ index.js
@@ -0,0 +1,144 @@
+const MongoClient = require('mongodb').MongoClient;
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const uuidv1 = require('uuid');
+const https = require('https')
+const s3 = new AWS.S3();
+
+const config = require('./config.json');
+
+//Use this code if your mongoDB use a certificate to connect
+/*
+const certFileBuf = fs.readFileSync('./ca.pfx');
+
+const options = {
+    sslCA: certFileBuf,
+    useUnifiedTopology: true
+}*/
+
+//MongoDB Configuration
+const mongodburl = config.mongodburl;
+const mongodbcollection = config.mongodbcollection;
+const mongodbdatabase = config.mongodbdatabase;
+const mongodbquery = config.mongodbquery;
+
+//Qrvey Configuration
+const s3Bucket = config.s3Bucket;
+const metadataId = config.metadataId;
+const qrveyPostdataUrl = config.postdataurl;
+const qrveyApiKey = config.apikey;
+
+//Init dataloading
+const initLoading = async () => {
+    return new Promise((resolve, reject) => {
+        const data = {
+            "datasetId": `ds_${collection}`,
+            "metadataId": metadataId,
+            "datasources": [
+                {
+                    "datasourceId": `dsource_${collection}`,
+                    "indexName": metadataId,
+                    "dataConnection": {
+                        "appid": `appid-${collection}`,
+                        "connectorid": `connector-${collection}`,
+                        "connectorType": "FILE_UPLOAD",
+                        "name": "JSON File Connector",
+                        "s3Bucket": dataBucket,
+                        "s3Path": folderName,
+                        "contentType": "application/json"
+                    }
+                }
+            ]
+        }
+        var options = {
+            hostname: qrveyPostdataUrl,
+            path: '/init',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': qrveyApiKey
+            },
+            method: 'POST',
+            port: 443
+        }
+        var req = https.request(options, function (res) {
+            var chunks = []
+            res.on('data', (chunk) => {
+                chunks.push(chunk.toString())
+            })
+            res.on('end', () => {
+                return resolve(chunks);
+            })
+            res.on('error', (error) => {
+                return reject(error);
+            });
+        })
+        var postData = JSON.stringify(data)
+        req.write(postData)
+        req.end()
+    });
+}
+
+//Connect to mongoDB database
+const connectToMongoDB = async () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const client = await MongoClient.connect(mongodburl, options);
+            const adminDb = client.db(mongodbdatabase);
+            const cursor = adminDb.collection(mongodbcollection);
+            const stream = cursor.find(mongodbquery).stream();
+            var batch = [];
+
+            stream.on('end', async () => {
+                if (batch.length > 0) {
+                    await loadDataToS3(batch);
+                }
+                console.log('Stream ended!');
+                client.close();
+                return resolve(true);
+            });
+
+            stream.on('data', async (doc) => {
+                batch.push(doc);
+
+                if (batch.length == 1000) {
+                    stream.pause();
+                    await loadDataToS3(batch);
+                    batch = [];
+                    stream.resume();
+                }
+            });
+            stream.on('error', error => {
+                client.close();
+                return reject(error);
+            });
+        } catch (error) {
+            client.close();
+            return reject(error);
+        }
+    });
+}
+
+//Load data to S3 bucket
+const loadDataToS3 = async (data) => {
+    const params = {
+        Body: JSON.stringify(data),
+        Bucket: s3Bucket,
+        ContentType: 'application/json',
+        Key: `${collection}/${uuidv1.v1()}.json`
+    };
+    await s3.upload(params).promise();
+}
+
+const run = async () => {
+    try {
+        await connectToMongoDB();
+        const jobId = await initLoading();
+        console.log('jobId: ', jobId);
+    } catch (error) {
+        console.log('error: ', error);
+    }
+
+}
+
+run();
+
